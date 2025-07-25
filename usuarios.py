4a9740153c056a57271b9955cc72589529affd2e
Efedeequis85 <d.aquino1@outlook.com>
2025-07-24 21:18:11 -0300

    Usuarios

---
+++ usuarios.py
@@ -0,0 +1,30 @@
+import requests
+from autenticacion import autenticacion
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+user = 'daquino@pam'
+passwd = 'meditecEsGenial22_'
+ip = '10.112.22.5'
+
+url_usuarios = f'https://{ip}:8006/api2/json/access/users'
+headers = autenticacion(user, passwd, ip)
+rta = requests.get(url=url_usuarios, headers=headers, verify=False)
+
+usuarios = rta.json()['data']
+
+for usuario in usuarios:
+	if 'firstname' in usuario:
+		firstname = usuario['firstname']
+	else:
+		firstname = ''
+	if 'lastname' in usuario:
+		lastname = usuario['lastname']
+	else:
+		lastname = ''
+	if 'email' in usuario:
+		email = usuario['email']
+	else:
+		email = ''
+
+	print(f'{usuario['userid']} - {firstname} - {lastname} - {email} - {usuario['enable']} - {usuario['realm-type']}')
